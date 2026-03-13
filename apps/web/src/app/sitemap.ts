7f1f4243e96955a5c881b505f9c35b10860632d4
Muhammed Sina Gün <mgun345@icloud.com>
2026-03-14 00:34:06 +0300

    Add dynamic export to sitemap.ts

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -1,4 +1,5 @@
-import type { MetadataRoute } from 'next';
+import type { MetadataRoute } from 'next';export const dynamic = 'force-static';
+
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const base = 'https://arelsoftwareclub.github.io';
